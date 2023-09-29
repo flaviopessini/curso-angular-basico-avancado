ba80d019cc29cb27ab349e9b957181ba3ee142da
flaviohp <flaviohenrique@lorensid.com>
2023-09-29 16:45:32 -0300

    Seção 8 - Aula 10

--- app-todo/src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ app-todo/src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
-  styleUrls: ['./todo-input-add-itens.component.scss']
+  styleUrls: ['./todo-input-add-itens.component.scss'],
 })
 export class TodoInputAddItensComponent {
+  @Output() emitItemTaskList = new EventEmitter();
 
+  addItemTaskList: string = '';
+
+  submitItemTaskList(): void {
+    this.addItemTaskList = this.addItemTaskList.trim();
+    if (this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = '';
+    }
+  }
 }
--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { TaskList } from '../../model/task-list';
+import { ComponentFixture } from '@angular/core/testing';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck {
+  taskList: Array<TaskList> = [];
 
+  ngDoCheck(): void {
+    this.taskList.sort(
+      (first, last) => Number(first.checked) - Number(last.checked)
+    );
+  }
+
+  deleteItemTaskList(event: number): void {
+    this.taskList.splice(event, 1);
+  }
+
+  deleteAllTask(): void {
+    const confirm = window.confirm(
+      'Você deseja realmente apagar todas as tarefas?'
+    );
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
+
+  setEmitTaskList(event: string): void {
+    this.taskList.push({ task: event, checked: false });
+  }
+
+  validationInput(event: string, index: number): void {
+    if (event.length <= 0) {
+      const confirm = window.confirm('Tarefa está vazia, deseja apagar?');
+      if (confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
 }
