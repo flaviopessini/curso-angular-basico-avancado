da7484beeaf758f45ba49e00e008a173605f3d76
flaviohp <flaviohenrique@lorensid.com>
2023-10-13 10:00:06 -0300

    Seção 18 - Acessibilidade simplificada no titulo da página

--- angular-standalone/src/app/app.routes.ts
+++ angular-standalone/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './core/components/home/home.component';
 import { SobreComponent } from './core/components/sobre/sobre.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'sobre', component: SobreComponent },
+  { path: '', component: HomeComponent, title: 'Início' },
+  { path: 'sobre', component: SobreComponent, title: 'Sobre' },
 ];
