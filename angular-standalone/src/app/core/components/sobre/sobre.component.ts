652f09a28d731bc5cb7be9c2bbdb8c5ff6f0582c
flaviohp <flaviohenrique@lorensid.com>
2023-10-13 09:58:40 -0300

    Seção 18 - reactive forms tipados

--- angular-standalone/src/app/core/components/sobre/sobre.component.ts
+++ angular-standalone/src/app/core/components/sobre/sobre.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sobre.component.html',
-  styleUrls: ['./sobre.component.scss']
+  styleUrls: ['./sobre.component.scss'],
 })
-export class SobreComponent {
+export class SobreComponent implements OnInit {
+  form = this.fb.group({
+    email: ['flavio@example.com'],
+    password: [''],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    console.log(this.form.value.email);
+  }
 }
